6d759447d81df19c67a4bf377ee344e244c9b53d
Aleksey Ivantsov <Lexin1n3@gmail.com>
2021-02-28 23:58:10 +0500

    Tests added on JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldAdviceWhenAmountNotEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAdviceWhenAmountEqualsZero() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAdviceZeroWhenAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
